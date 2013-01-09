c700eef492a481e7e1341b6b950cbaa91c19962e
Raul Sanchez <raul@um.es>
2013-01-09 13:13:57 +0100

    initial app with cas working

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :cas_authenticatable, :rememberable, :trackable, :lockable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :username, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bambi::Application.config.secret_token = '4f2c47125fdd1f763186e99d83dc0693de480b8f1733c6f7dcfef8c338f981ad3a9b3eb9d09fb0a6c0a1b3e095f519c8833170b0e01d7e4078f23b362d4c7308'
