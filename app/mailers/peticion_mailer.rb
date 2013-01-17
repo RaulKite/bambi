e02dc54951d95ab998c5743392fb2f783a7a11b4
Raul Sanchez <raul@um.es>
2013-01-17 14:57:44 +0100

    mailer for peticionsoftware working

--- app/mailers/peticion_mailer.rb
+++ app/mailers/peticion_mailer.rb
@@ -2,6 +2,7 @@ class PeticionMailer < ActionMailer::Base
   default from: "ccalculo@um.es"
 
   def peticionrealizada_email(peticion)
+    @peticion = peticion
     mail(:to => peticion.user.username, :subject => "Peticion de Instalacion de software")
   end
 end
