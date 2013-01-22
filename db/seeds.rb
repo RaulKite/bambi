f1a99f2f8b76d0d589ed647a1777dee2b82f2b14
Raul Sanchez <raul@um.es>
2013-01-22 12:26:02 +0100

    seeds laboratorios and cursoacademico

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+Laboratorio.create(:name => "Laboratorio 1.1", :description => "Planta Baja")
+Laboratorio.create(:name => "Laboratorio 1.2", :description => "Planta Baja")
+Laboratorio.create(:name => "Laboratorio 1.3", :description => "Planta Baja")
+Laboratorio.create(:name => "Laboratorio 1.4", :description => "Planta Baja")
+Laboratorio.create(:name => "Laboratorio 1.5", :description => "Planta Baja")
+Laboratorio.create(:name => "Laboratorio 1.6", :description => "Planta Baja")
+Laboratorio.create(:name => "Laboratorio 1.7", :description => "Planta Baja")
+Laboratorio.create(:name => "Laboratorio 2.0", :description => "Planta Primera")
+Laboratorio.create(:name => "Laboratorio 2.1", :description => "Planta Primera")
+Laboratorio.create(:name => "Laboratorio 2.2", :description => "Planta Primera")
+Laboratorio.create(:name => "Laboratorio 2.3", :description => "Planta Primera")
+Laboratorio.create(:name => "Laboratorio 2.5", :description => "Planta Primera")
+Laboratorio.create(:name => "Laboratorio 2.7", :description => "Planta Primera")
+Laboratorio.create(:name => "Laboratorio 2.8", :description => "Planta Primera")
+
+Cursoacademico.create(:name => "2012/2013")
+
