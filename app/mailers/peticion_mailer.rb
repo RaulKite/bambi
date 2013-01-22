69bc90c32160f5dd785da1735abb596434e646d6
Raul Sanchez <raul@um.es>
2013-01-22 12:55:07 +0100

    mail bcc to ccaclculo

--- app/mailers/peticion_mailer.rb
+++ app/mailers/peticion_mailer.rb
@@ -3,6 +3,8 @@ class PeticionMailer < ActionMailer::Base
 
   def peticionrealizada_email(peticion)
     @peticion = peticion
-    mail(:to => peticion.user.username, :subject => "Peticion de Instalacion de software")
+    mail(:to => peticion.user.username, 
+         :bcc => "ccalculo@um.es",
+         :subject => "Peticion de Instalacion de software")
   end
 end
